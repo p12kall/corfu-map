74f32ef82df67913a6f35f10dec730d64eebb49f
AndreasKalli <p12kall@users.noreply.github.com>
2018-02-26 23:57:29 +0200

    Update editor.js

--- editor.js
+++ editor.js
@@ -9,9 +9,9 @@ function init() {
   map = new google.maps.Map(document.getElementById('map-holder'), {
     center: {lat: 39.625555, lng: 19.925555},
     zoom: 6,
-    mapTypeControl: false,
-    streetViewControl: false,
-    fullscreenControl: false,
+    //mapTypeControl: false,
+  //  streetViewControl: false,
+//    fullscreenControl: false,
     mapTypeId: 'satellite'
   });
 	
